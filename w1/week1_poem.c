abcfd8f48889efa403eb783b05302d872b79b302
kaba64 <Kazem Bazesefidpar>
2023-10-08 18:46:45 +0200

    The assignments of week1 done!

---
+++ w1/week1_poem.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void){
+  printf("\nLittle drops of water,\nLittle grains of sand,\nMake the mighty ocean\nAnd the pleasant land.\n\n&By Julia Abigail Fletcher Carney\n");
+    return 0;
+}
